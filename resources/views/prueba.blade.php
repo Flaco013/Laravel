76ee01f183d66d0c9599a4c9d866152c6ca4ac93
Flaco013 <alexisgasga7@gmail.com>
2023-11-04 22:51:07 -0700

    Route name/numberAddition/View with name

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Hello, {{ $name }}</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Expedita fugiat sunt asperiores voluptatum, reiciendis facilis ut autem. Libero, maxime nemo 
+    excepturi odit deserunt quod consectetur animi minus soluta harum corrupti.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/busca-usuario/{name}/{lastname}", function($name, $lastname = "doe"){
+ return "Hola: " .$name. " " .$lastname;
+
+}) -> where(["name" => "[A-Za-z]+", "lastname" => "[A-Za-z]+"])->name("filtrado");
+
+Route::get("/suma/{num1}/{num2}", function($num1, $num2){
+return $num1 + $num2;
+}) -> where(["num1"=>"[0-9]+","num2"=>"[0-9]+"]) ->name("suma");
+
+
+Route:: get("/vista/{name}", function($name){
+    return view("prueba", ["name" => $name]);
+});
